7cf3c5bbc9de75c78de304691eee33df7f2e0c9d
Libin Salal <libin.salal@dominos.com>
2016-06-17 14:16:49 -0400

    Set custom action mode callback and returing false to prevent the action mode from showing on text selection

--- app/src/main/java/com/libin/emaileditview/EmailEditView.java
+++ app/src/main/java/com/libin/emaileditview/EmailEditView.java
@@ -2,6 +2,9 @@ package com.libin.emaileditview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 /**
@@ -11,14 +14,17 @@ public class EmailEditView extends EditText {
 
     public EmailEditView(Context context) {
         super(context);
+        init();
     }
 
     public EmailEditView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public EmailEditView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @Override
@@ -32,4 +38,27 @@ public class EmailEditView extends EditText {
         return super.onTextContextMenuItem(id);
     }
 
+    private void init() {
+        setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                // NOP
+            }
+        });
+    }
 }
