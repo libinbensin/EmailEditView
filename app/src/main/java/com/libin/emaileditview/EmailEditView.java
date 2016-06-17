b3ea33b329dd39c0073d48ca4e4ebd3c3dd1545c
Libin Salal <libin.salal@dominos.com>
2016-06-17 14:42:47 -0400

    Added java doc

--- app/src/main/java/com/libin/emaileditview/EmailEditView.java
+++ app/src/main/java/com/libin/emaileditview/EmailEditView.java
@@ -1,6 +1,7 @@
 package com.libin.emaileditview;
 
 import android.content.Context;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.ActionMode;
 import android.view.Menu;
@@ -8,6 +9,16 @@ import android.view.MenuItem;
 import android.widget.EditText;
 
 /**
+ * Custom view to disable the copy paste feature on {@link EditText} used for entering Email address.
+ * This will block the text paste from clipboard , but you will still able to see the paste menu when tapped on cursor bubble.
+ *
+ * - Long click is disabled {@link #setLongClickable(boolean)}
+ * - Input type set to Email Address {@link InputType#TYPE_TEXT_VARIATION_EMAIL_ADDRESS}
+ * - Set custom selection action to prevent the selection action mode from being showing.
+ *
+ * @see {@link android.R.id#paste}
+ *  @see {@link android.R.id#pasteAsPlainText}
+ *
  * @author Libin
  */
 public class EmailEditView extends EditText {
@@ -30,6 +41,7 @@ public class EmailEditView extends EditText {
     @Override
     public boolean onTextContextMenuItem(int id) {
         switch (id){
+            // Return false to prevent paste when paste menu tapped.
             case android.R.id.paste:
             case android.R.id.pasteAsPlainText:
                 return false;
@@ -39,6 +51,11 @@ public class EmailEditView extends EditText {
     }
 
     private void init() {
+        // Set the inout type as email address , so that the keyboard will show @ key.
+        setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        // Disable long clickable.
+        setLongClickable(false);
+        // Set custom selection action to prevent the selection action mode from being showing.
         setCustomSelectionActionModeCallback(new ActionMode.Callback() {
             @Override
             public boolean onCreateActionMode(ActionMode mode, Menu menu) {
